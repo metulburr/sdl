874bb29853d27eadd1c752ad34acdc077373650d
metulburr <micahpage911@yahoo.com>
2013-12-01 04:24:23 -0500

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -1,7 +1,8 @@
+
 #include <iostream>
 #include "SDL/SDL.h"
 
-// -lSDL
+
 
 class Window{
     public:
@@ -34,10 +35,74 @@ class Window{
             return true;
             
         }
+        
+        bool resize(int width, int height){
+            this->width = width;
+            this->height = height;
+            if ((_window = SDL_SetVideoMode(width, height, bpp, flags)) == NULL){
+                return false;
+            }
+            return true;
+        }
+};
+
+class Input{
+    public:
+        Input(){
+            
+        }
+        
+        void on_event(SDL_Event* event){
+            switch(event->type){
+                case SDL_KEYDOWN:
+                    key_press(event->key.keysym.sym, 
+                        event->key.keysym.mod, 
+                        event->key.keysym.unicode);
+                    break;
+                case SDL_KEYUP:
+                    key_release(event->key.keysym.sym, 
+                        event->key.keysym.mod, 
+                        event->key.keysym.unicode);
+                    break;
+                case SDL_MOUSEMOTION:
+                    mouse_motion(event->motion.x, event->motion.y,
+                        event->motion.xrel, event->motion.yrel,
+                        (event->motion.state & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0,
+                        (event->motion.state & SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0,
+                        (event->motion.state & SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0);
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    mouse_press(event->button.button, event->button.x, event->button.y);
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    mouse_release(event->button.button, event->button.x, event->button.y);
+                    break;
+                case SDL_ACTIVEEVENT:
+                    focus_change(event->active.state, event->active.gain);
+                    break;
+                case SDL_VIDEORESIZE:
+                    resize(event->resize.w, event->resize.h);
+                    break;
+                case SDL_QUIT:
+                    exiting();
+                    break;
+            }
+        }
+        
+        virtual void key_press(SDLKey key, SDLMod mod, Uint16 unicode){}
+        virtual void key_release(SDLKey key, SDLMod mod, Uint16 unicode){}
+        virtual void mouse_motion(int x, int y, int relx, int rely, bool left,  bool middle, bool right){}
+        virtual void mouse_press(Uint8 button, int x, int y){}
+        virtual void mouse_release(Uint8 button, int x, int y){}
+        virtual void focus_change(Uint8 state, Uint8 gain){}
+        virtual void resize(int width, int height){}
+        virtual void exiting(){}
+
 };
 
 
-class Application{
+
+class Application : public Input{
     public:
         bool done;
         Window window;
@@ -45,7 +110,7 @@ class Application{
         Application(){
             done = false;
         }
-        ~Application(){
+        virtual ~Application(){
             
         }
     
@@ -61,16 +126,20 @@ class Application{
             return true;
         }
         void run(){
+            SDL_Event event;
             while( ! done){
-                SDL_Delay(5000);
-                done = true;
+                input(&event);
+                update();
+                render();
             }
         }
         void input(SDL_Event* event){
-            
+            while(SDL_PollEvent(event)){
+                on_event(event);
+            }
         }
         void update(){
-            
+            SDL_Delay(50);
         }
         void render(){
             
@@ -78,7 +147,17 @@ class Application{
         void quit(){
             SDL_Quit();
         }
+        
+        void resize(int width, int height){
+            if ( ! window.resize(width, height)){
+                exiting();
+            }
+        }
+        void exiting(){
+            done = true;
+        }
 };
+
 
 
 int main(){
